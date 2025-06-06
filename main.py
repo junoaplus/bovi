fcdf397deec03d8346d6962553f55d931819e2c7
junoaplus <junehoo4248@naver.com>
2025-06-06 18:20:15 +0900

    3

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8888,
+        port=8000,
         reload=True,
         log_level="info"
     )
